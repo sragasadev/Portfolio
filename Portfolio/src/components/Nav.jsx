8cc58be723404e25786b823e77c85cb5bee18c56
Shaun <sragasa97@gmail.com>
2023-05-01 12:59:25 -0400

    Installed SVGR plugin, added nav bar square logos, and deleted unneccessary initial setup files

--- Portfolio/src/components/Nav.jsx
+++ Portfolio/src/components/Nav.jsx
@@ -1,6 +1,13 @@
 import { useState, useRef, useEffect } from "react";
-import { NavLink } from "react-router-dom";
 import PortfolioIcon from "../assets/portfolio_icon.png";
+import { ReactComponent as LinkedInIcon } from "../assets/linkedin-icon.svg";
+import { ReactComponent as GitlabIcon } from "../assets/gitlab-icon.svg";
+import { ReactComponent as ResumeIcon } from "../assets/resume-icon.svg";
+
+// import { ReactComponent as LinkedInSquare } from "../assets/linkedin-square.svg";
+// import { ReactComponent as GitlabSquare } from "../assets/gitlab-square.svg";
+// import { ReactComponent as ResumeSquare } from "../assets/resume-square.svg";
+
 import "../App.css";
 
 function Nav() {
@@ -40,18 +47,41 @@ function Nav() {
 				className={`
 				${visible ? "visible" : ""}`}
 			>
-				<img className="h-8 w-8 rounded-lg -ml-1" src={PortfolioIcon} alt="Portfolio Icon" />
-				<div className="flex items-center gap-5 ">
+				<div>
+					<img className="h-8 w-8 rounded-lg -ml-1" src={PortfolioIcon} alt="Portfolio Icon" />
+				</div>
+				<div className="flex items-center justify-around gap-5">
 					<h2 className="nav-item">About</h2>
 					<h2 className="nav-item">Skills</h2>
 					<h2 className="nav-item">Projects</h2>
 					<h2 className="nav-item">Resume</h2>
 					<h2 className="nav-item">Contact</h2>
 				</div>
-				<div className="flex align-middle justify-around gap-6 rounded-lg nav-logo">
-					<div>1</div>
-					<div>2</div>
-					<div>3</div>
+				{/* <div className="w-screen flex items-center justify-around">
+					<LinkedInSquare className="h-8" />
+					<GitlabSquare className="h-8" />
+					<ResumeSquare className="h-8" />
+				</div> */}
+				<div className="flex items-center justify-around gap-2">
+					<a
+						href="https://www.linkedin.com/in/shaunragasa/"
+						target="_blank"
+						rel="noopener noreferrer"
+						alt="LinkedIn"
+					>
+						<LinkedInIcon className="nav-logo" />
+					</a>
+					<a href="https://gitlab.com/sragasa97" target="_blank" rel="noopener noreferrer" alt="Gitlab">
+						<GitlabIcon className="nav-logo" />
+					</a>
+					<a
+						href="https://drive.google.com/file/d/1q1Dq2DOt4etw1dQTqOzg9ucIMk57d7Sp/view?usp=sharing"
+						target="_blank"
+						rel="noopener noreferrer"
+						alt="Resume"
+					>
+						<ResumeIcon className="nav-logo" />
+					</a>
 				</div>
 			</nav>
 		</>
--- Portfolio/vite.config.js
+++ Portfolio/vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
-    },
-    host: true, // lets Docker Container port mapping work
-    strictPort: true,
-    port: 5173, // default Vite port
-  }
-})
+	plugins: [svgr(), react()],
+	server: {
+		watch: {
+			usePolling: true,
+		},
+		host: true, // lets Docker Container port mapping work
+		strictPort: true,
+		port: 5173, // default Vite port
+	},
+});
