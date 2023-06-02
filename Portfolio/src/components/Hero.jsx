b9a1365502f1a69af3bbeb6a147e3f0a34e44e3d
Shaun <sragasa97@gmail.com>
2023-06-02 17:33:04 -0400

    Added temporary hero section

---
+++ Portfolio/src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import "../App.css";
+
+function Hero() {
+	return <h1 className="text-3xl bg-[#FAF3DD] pt-48 w-full text-center">DEVELOPMENT IN PROGRESS</h1>;
+}
+
+export default Hero;
+
+{
+	/* <section id="hero" className="h-[85svh] w-full border-2 border-red-400 ">
+	<h2 className="mt-[15svh]">Hey! My name is Shaun Ragasa.</h2>
+	<h1 className="">I'm a _____.</h1>
+	<h2 className="">*Description*</h2>
+	<button>
+		<a href="https://drive.google.com/file/d/1PjY6bqQEBeuHqvUC4zZAjB1nhtxbTqUs/view?usp=sharing">Hire Me!</a>
+	</button>
+</section>; */
+}
