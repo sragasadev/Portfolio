9533ec4b7449431a6fc2d9ca40c9795845ba34a4
Shaun <sragasadev@gmail.com>
2023-07-27 23:25:51 -0400

    Began filling in recommendations

--- Portfolio/src/components/Recommendations.jsx
+++ Portfolio/src/components/Recommendations.jsx
@@ -1,4 +1,16 @@
 function Recommendations() {
+	const recommendations = [
+		{
+			name: "Alana Torrez",
+			title: "Full Stack Software Engineer",
+			linkedin:
+				"https://www.linkedin.com/in/alana-torrez?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_recommendations_details%3BU%2FmiqyuUTfewxstSsBRA6A%3D%3D",
+			picture: "",
+			recommendation:
+				"I had the incredible privilege of being on the same team as Shaun during our recent project in Hack Reactor's rigorous 19-week Software Engineering immersive program. Throughout our collaboration, Shaun consistently stood out as an exceptional teammate, demonstrating a remarkable dedication to helping others and a passion for understanding the intricacies of our work. Even before we officially became teammates, Shaun's willingness to assist struggling classmates distinguished him from the cohort of over 70 students. Beyond simply providing answers, Shaun took the time to ensure his peers grasped the underlying concepts and left with a deeper comprehension of the subject matter. Shaun's ability to help others stems from his genuine curiosity about the inner workings of systems and his unwavering commitment to sharing knowledge. Shaun's keen eye for detail made a significant impact on both the frontend and backend design of our project. He consistently went above and beyond, aligning the application's look and feel with the overarching vision of our team. His meticulousness and dedication brought an exceptional level of polish to our work. Furthermore, Shaun's troubleshooting abilities were truly unparalleled. On multiple occasions, he made crucial logical connections that enabled us to overcome obstacles and propel our progress forward. Shaun's ability to identify and solve complex problems swiftly and effectively played a pivotal role in the success of our project. Working with Shaun was an absolute delight, and I have no doubt that any organization fortunate enough to have him as part of their team will benefit tremendously. His genuine commitment to helping others, his unwavering pursuit of excellence, and his ability to navigate challenging situations make him an invaluable asset. Shaun's contributions will undoubtedly strengthen any team and drive success in future endeavors.",
+		},
+	];
+
 	return (
 		<section
 			id="recommendations"
