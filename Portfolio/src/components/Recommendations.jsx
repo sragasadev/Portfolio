9fb95a5a9dec2bbec180e6a365d433064630e7d1
Shaun <sragasadev@gmail.com>
2023-07-29 19:33:52 -0400

    Began coding recommendations

--- Portfolio/src/components/Recommendations.jsx
+++ Portfolio/src/components/Recommendations.jsx
@@ -54,11 +54,13 @@ function Recommendations() {
 		>
 			<h1>RECOMMENDATIONS</h1>
 			<h1>🚧 CURRENTLY IN DEVELOPMENT 🚧</h1>
-			<p>
-				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit, cum quas dolore, eos harum quibusdam
-				reprehenderit eaque ducimus, sit error tenetur cupiditate veritatis illo illum accusantium beatae eum
-				rem fugit.
-			</p>
+			<div className="flex">
+				{recommendations.map((rec, index) => (
+					<div className="" key={index}>
+						{rec.name}
+					</div>
+				))}
+			</div>
 		</section>
 	);
 }
